eb74c9b0c5da02f799e4f7f8656b7a4a7c22ba7f
yuyaowen <563570587@qq.com>
2019-10-21 17:23:09 +0800

    Fix pt-active syntax

--- tracker.sh
+++ tracker.sh
@@ -6,19 +6,19 @@ LOCKFILE=/tmp/track_generic_move_netcam.lock
 
 if [ "$TRACK_ACTION"x == "move"x ]; then
 	if [ "$TRACK_CENT_X" -lt "$((TRACK_IMGS_WIDTH*3/8))" ]; then
-		pt-active left
+		pt-active left high
 	fi
 
 	if [ "$TRACK_CENT_X" -gt "$((TRACK_IMGS_WIDTH*5/8))" ]; then
-		pt-active right
+		pt-active right high
 	fi
 
 	if [ "$TRACK_CENT_Y" -lt "$((TRACK_IMGS_HEIGHT*3/8))" ]; then
-		pt-active up
+		pt-active up high
 	fi
 
 	if [ "$TRACK_CENT_Y" -gt "$((TRACK_IMGS_HEIGHT*5/8))" ]; then
-		pt-active down
+		pt-active down high
 	fi
 fi
 
